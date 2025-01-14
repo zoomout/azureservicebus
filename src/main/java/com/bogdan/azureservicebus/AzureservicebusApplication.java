42c3a19e5210c73bd6b4d54d4159721735965969
zoomout <zoomout@users.noreply.github.com>
2025-01-14 16:29:56 +0100

    generated project

---
+++ src/main/java/com/bogdan/azureservicebus/AzureservicebusApplication.java
@@ -0,0 +1,13 @@
+package com.bogdan.azureservicebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureservicebusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureservicebusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bogdan/azureservicebus/AzureservicebusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogdan.azureservicebus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureservicebusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
